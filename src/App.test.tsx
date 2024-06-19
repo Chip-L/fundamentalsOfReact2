14ce1c8482da5e745ca2da9a124c187be93c1f94
Chip Long <chip_long@yahoo.com>
2024-06-19 10:38:11 -0600

    working app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,91 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
+type mockResponseProps = {
+  ok?: boolean;
+  json: string;
+  status?: number;
+  statusText?: string;
+};
+
+const getMockResponse = ({
+  ok = true,
+  json,
+  status = 200,
+  statusText = "",
+}: mockResponseProps): Response => {
+  return {
+    json: vi.fn().mockResolvedValue(json),
+    ok,
+    status,
+    statusText,
+    headers: {} as Headers,
+    redirected: false,
+    type: "cors",
+    url: "",
+    clone: vi.fn(),
+    body: {} as ReadableStream<Uint8Array>,
+    bodyUsed: false,
+    arrayBuffer: vi.fn(),
+    blob: vi.fn(),
+    formData: vi.fn(),
+    text: vi.fn(),
+  };
+};
+
+const fetchSpy = vi.spyOn(global, "fetch").mockResolvedValue(
+  getMockResponse({
+    json: JSON.stringify([
+      {
+        id: "ba6",
+        url: "https://cdn2.thecatapi.com/images/ba6.jpg",
+        width: 500,
+        height: 331,
+      },
+    ]),
+  })
+);
+
+function renderApp() {
+  render(<App />);
+}
+
 describe("something truthy and falsy", () => {
-  it("displays a cat image", () => {
-    render(<App />);
-    expect(screen.getByText("hello world")).toBeInTheDocument();
+  beforeEach(() => {
+    vi.clearAllMocks();
   });
 
-  it("false to be false", () => {
-    expect(false).toBe(false);
+  it("displays loading (while getting image)", () => {
+    renderApp();
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    expect(screen.queryByRole("img")).not.toBeInTheDocument();
+    expect(screen.queryByRole("img")).not.toBeInTheDocument();
+  });
+
+  it("displays a cat image", () => {
+    renderApp();
+
+    const loading = screen.getByText(/loading/i);
+    const image = screen.queryByRole("img");
+
+    waitFor(() => {
+      expect(loading).not.toBeInTheDocument();
+      expect(image).toBeInTheDocument();
+      expect(image).toHaveAttribute(
+        "src",
+        "https://cdn2.thecatapi.com/images/ba6.jpg"
+      );
+      expect(image).toHaveAttribute("alt", "cat image");
+    });
+    expect(fetchSpy).toHaveBeenCalled();
+  });
+
+  it("displays a generate button", () => {
+    renderApp();
+
+    const btn = screen.getByRole("button", { name: "Get cat" });
+
+    expect(btn).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const url = "https://cdn2.thecatapi.com/images/ba6.jpg";
+  const [url, setUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const getUrl = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const json = await response.json();
+
+    if (response.ok) {
+      setUrl(json[0].url);
+    } else {
+      setError(`Error: ${json.message}`);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getUrl();
+  }, []);
 
   return (
-    <>
-      <img src={url} alt="cat image" />
-    </>
+    <div style={{ display: "flex", flexDirection: "column", width: "800px" }}>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          {error ? (
+            <p style={{ color: "red" }}>{error}</p>
+          ) : (
+            url && <img src={url} alt="cat image" />
+          )}
+        </>
+      )}
+      <button onClick={() => getUrl()}>Get cat</button>
+    </div>
   );
 }
 
